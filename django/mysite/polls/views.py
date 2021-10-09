9a17aee3115f1c7fcc315079d26b4f1ccb7f382e
Leonardwkwong <leonard_wong@live.com>
2021-10-09 22:07:38 +0800

    Committing full project into Github

---
+++ django/mysite/polls/views.py
@@ -0,0 +1,60 @@
+#from django.http import HttpResponse
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. c7a37dd1 is the polls index.")
+
+#def owner(request):
+#    return HttpResponse("Hello, world. d96988b8 is the polls index.")
+
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+
+from .models import Choice, Question
+
+def owner(request):
+    return HttpResponse("Hello, world. 484ef890 is the polls index.")
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
